b75034ac29d181be703e7a328090277e36295d52
7Mica <femicasillasur@ittepic.edu.mx>
2020-12-19 20:12:31 -0700

    Added tool bar styles for user's module.

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-page',
   templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss'],
 })
 export class UserComponent {}
--- src/app/pages/user/user.module.ts
+++ src/app/pages/user/user.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { DialogDataExchangeModule } from 'src/app/providers/dialog-data-exchange/dialog-data-exchange.module';
 import { DialogDataExchange } from 'src/app/providers/dialog-data-exchange/dialog-data-exchange.service';
@@ -12,6 +13,7 @@ import { USER_COMPONENT_ROUTES } from './user.routes';
     RouterModule.forChild(USER_COMPONENT_ROUTES),
     ListUsersModule,
     DialogDataExchangeModule.forRoot(new DialogDataExchange()),
+    MatIconModule,
   ],
   providers: [],
   exports: [RouterModule],
